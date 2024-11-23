493012b7e1ec2988478862c5eef6cd5808fd335b
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2024-11-23 16:54:21 +0800

    add more Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  reactStrictMode: false,
+  compress: true,
+  poweredByHeader: false
 };
 
 export default nextConfig;
