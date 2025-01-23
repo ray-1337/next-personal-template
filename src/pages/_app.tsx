23189f71e707f8b89b13d3267fd49a7f992d71ab
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-01-23 22:47:28 +0800

    i complicate a simple thing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,21 +4,11 @@ import Head from "next/head";
 
 import "normalize.css";
 
-const viewport = {
-  width: "device-width",
-  height: "device-height",
-  "user-scalable": "no",
-  "shrink-to-fit": "no",
-  "initial-scale": "1.0"
-};
-
-const viewportContent = Object.entries(viewport).map((val) => `${val[0]}=${val[1]}`).join(", ");
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Head>
-        <meta name="viewport" content={viewportContent} />
+        <meta name="viewport" content={"width=device-width, height=device-height, user-scalable=no, shrink-to-fit=no, initial-scale=1.0"} />
       </Head>
       
       <Component {...pageProps} />
