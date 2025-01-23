a78bc5bb3d83f1b0eadebd7d0ca9fb358e1a0a9f
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-01-23 17:46:29 +0800

    add viewport meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,27 @@
+import { Fragment } from "react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 import "normalize.css";
 
+const viewport = {
+  width: "device-width",
+  height: "device-height",
+  "user-scalable": "no",
+  "shrink-to-fit": "no",
+  "initial-scale": "1.0"
+};
+
+const viewportContent = Object.entries(viewport).map((val) => `${val[0]}=${val[1]}`).join(", ");
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        <meta name="viewport" content={viewportContent} />
+      </Head>
+      
+      <Component {...pageProps} />
+    </Fragment>
+  );
 };
